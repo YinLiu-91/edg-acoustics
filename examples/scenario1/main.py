e5d90c02be0cb2d475c3bf3fc7780cb5507892e1
liuyin <2274882591@qq.com>
2025-04-14 22:03:03 +0800

    添加allclose,确定golden

--- examples/scenario1/main.py
+++ examples/scenario1/main.py
@@ -1,10 +1,13 @@
-""" This is the main script for the scenario 1."""
+"""This is the main script for the scenario 1."""
 
 # region Import Libraries
 import os
 import glob
 import numpy
 import scipy.io
+import sys
+
+sys.path.insert(0, "/media/liu/trHollow/linux/decimer/edg-acoustics")
 import edg_acoustics
 
 # endregion
@@ -25,7 +28,9 @@ real_valued_impedance_boundary = [
 ]  # extra labels for real-valued impedance boundary condition, if needed. The label should be the similar to the label in BC_labels. Since it's frequency-independent, only "RI", the real-valued reflection coefficient, is required. If not needed, just clear the elements of this list and keep the empty list.
 
 mesh_name = "scenario1_coarser.msh"  # name of the mesh file. The mesh file should be in the same folder as this script.
-monopole_xyz = numpy.array([3.04, 2.59, 1.62])  # x,y,z coordinate of the source in the room
+monopole_xyz = numpy.array(
+    [3.04, 2.59, 1.62]
+)  # x,y,z coordinate of the source in the room
 freq_upper_limit = 200  # upper limit of the frequency content of the source signal in Hz. The source signal is a Gaussian pulse with a frequency content up to this limit.
 
 # Approximation degrees
@@ -37,7 +42,7 @@ recy = numpy.array([1.76])
 recz = numpy.array([1.62])
 rec = numpy.vstack((recx, recy, recz))  # dim:[3,n_rec]
 
-impulse_length = 2  # total simulation time in seconds
+impulse_length = 0.005  # total simulation time in seconds
 save_every_Nstep = 10  # save the results every N steps
 temporary_save_Nstep = 500  # save the results every N steps temporarily during the simulation. The temporary results will be saved in the root directory of this repo.
 
@@ -53,7 +58,9 @@ for material, label in BC_labels.items():
     if material == "hard wall":
         BC_para.append({"label": label, "RI": 1})
     else:
-        mat_files = glob.glob(f"{os.path.split(os.path.abspath(__file__))[0]}/{material}*.mat")
+        mat_files = glob.glob(
+            f"{os.path.split(os.path.abspath(__file__))[0]}/{material}*.mat"
+        )
 
         # if mat_files is empty, raise an error
         if not mat_files:
@@ -71,9 +78,19 @@ for material, label in BC_labels.items():
 
         if "AS" in mat_file and "lambdaS" in mat_file:
             material_dict["RP"] = numpy.array([mat_file["AS"][0], mat_file["lambdaS"][0]])  # type: ignore
-        if "BS" in mat_file and "CS" in mat_file and "alphaS" in mat_file and "betaS" in mat_file:
+        if (
+            "BS" in mat_file
+            and "CS" in mat_file
+            and "alphaS" in mat_file
+            and "betaS" in mat_file
+        ):
             material_dict["CP"] = numpy.array(  # type: ignore
-                [mat_file["BS"][0], mat_file["CS"][0], mat_file["alphaS"][0], mat_file["betaS"][0]]
+                [
+                    mat_file["BS"][0],
+                    mat_file["CS"][0],
+                    mat_file["alphaS"][0],
+                    mat_file["betaS"][0],
+                ]
             )
 
         BC_para.append(material_dict)
@@ -113,8 +130,16 @@ results = edg_acoustics.Monopole_postprocessor(sim, 1)
 results.apply_correction()
 
 
-result_filename = os.path.join(os.path.split(os.path.abspath(__file__))[0], result_filename)
-results.write_results(result_filename, "mat")
+result_filename = os.path.join(
+    os.path.split(os.path.abspath(__file__))[0], result_filename
+)
+# results.write_results(result_filename, "mat")
+
+mat_data = scipy.io.loadmat(f"{result_filename}.mat")
+tr = mat_data["TR"]
+ir = mat_data["IR"]
+assert numpy.allclose(tr, results.TR)
+assert numpy.allclose(ir, results.IRnew)
 # load newresult.npy
 # data = numpy.load("./examples/newresult.npz", allow_pickle=True)
 # tempdata = numpy.load("./results_on_the_run.npz", allow_pickle=True)
